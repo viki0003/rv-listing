5c61f82350f0971ed9d4185771054cdfcdd8dd06
Subham11258 <subhamguptaslg.1996@gmail.com>
2025-04-03 00:28:22 +0530

    Add scroll to vehicle info

--- src/Components/ProductDetails/VehicleDetails/VehicleDetails.js
+++ src/Components/ProductDetails/VehicleDetails/VehicleDetails.js
@@ -8,34 +8,36 @@ const VehicleDetails = ({ product }) => {
     <div className="wrapper">
       <div className="container">
         <h2>Vehicle Details</h2>
-        <table className="details">
-          <tbody>
-            <tr>
-              <td className="td-name">Stock</td>
-              <td>{product.stock || "N/A"}</td>
-              <td className="td-name">VIN</td>
-              <td>{product.vin || "N/A"}</td>
-            </tr>
-            <tr>
-              <td className="td-name">Miles</td>
-              <td>{product.miles || "N/A"}</td>
-              <td className="td-name">Length</td>
-              <td>{product.length ? `${product.length} ft` : "N/A"}</td>
-            </tr>
-            <tr>
-              <td className="td-name">Interior Height</td>
-              <td>{product.interior_height ? `${product.interior_height} ft` : "N/A"}</td>
-              <td className="td-name">Height</td>
-              <td>{product.height ? `${product.height} ft` : "N/A"}</td>
-            </tr>
-            <tr>
-              <td className="td-name">Fuel Type</td>
-              <td>{product.fuel_type || "N/A"}</td>
-              <td className="td-name">Engine</td>
-              <td>{product.engine || "N/A"}</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="details-wrapper"> {/* Add this wrapper for scroll */}
+          <table className="details">
+            <tbody>
+              <tr>
+                <td className="td-name">Stock</td>
+                <td>{product.stock || "N/A"}</td>
+                <td className="td-name">VIN</td>
+                <td>{product.vin || "N/A"}</td>
+              </tr>
+              <tr>
+                <td className="td-name">Miles</td>
+                <td>{product.miles || "N/A"}</td>
+                <td className="td-name">Length</td>
+                <td>{product.length ? `${product.length} ft` : "N/A"}</td>
+              </tr>
+              <tr>
+                <td className="td-name">Interior Height</td>
+                <td>{product.interior_height ? `${product.interior_height} ft` : "N/A"}</td>
+                <td className="td-name">Height</td>
+                <td>{product.height ? `${product.height} ft` : "N/A"}</td>
+              </tr>
+              <tr>
+                <td className="td-name">Fuel Type</td>
+                <td>{product.fuel_type || "N/A"}</td>
+                <td className="td-name">Engine</td>
+                <td>{product.engine || "N/A"}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
 
         <div className="description">
           <h3>Vehicle Description</h3>
